79ef5e75940897109b81005e60269bc9d086316d
kalmantomika <kalman.tamaskrisztian@gmail.com>
2025-06-14 13:47:49 +0200

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: ['firebasestorage.googleapis.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        pathname: '/v0/b/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
