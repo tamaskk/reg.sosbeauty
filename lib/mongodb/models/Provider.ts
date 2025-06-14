ebc1b57d8158428866f2e80cd7cfa3a6f90d8742
kalmantomika <kalman.tamaskrisztian@gmail.com>
2025-06-14 13:26:53 +0200

    fix

--- lib/mongodb/models/Provider.ts
+++ lib/mongodb/models/Provider.ts
@@ -32,7 +32,7 @@ export interface IProvider extends Document {
 
 const ProviderSchema: Schema = new Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: false },
   category: { 
     type: String, 
     required: true,
